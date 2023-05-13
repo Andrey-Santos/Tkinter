d3d1416de3b4685c67a9aa1004c0da148caefb24
Andrey C. Dos Santos <Andrey-Carlos@outlook.com>
2023-05-13 17:53:24 -0300

    Criado Frames

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ class Application():
     def __init__(self):
         self.root = root
         self.tela()
-
+        self.frames_da_tela()
         self.root.mainloop()
 
     def tela(self):
@@ -17,6 +17,10 @@ class Application():
         self.root.maxsize(width = 900, height = 700)   
         self.root.minsize(width = 400, height = 300)   
 
-     
+    def frames_da_tela(self):
+        self.frame1 = Frame(self.root,bd=4,bg='#dfe3ee',highlightbackground='#759fe6',highlightthickness=3) 
+        self.frame1.place(relx=0.02, rely=0.02, relwidth=0.96, relheight=0.46)       
+        self.frame2 = Frame(self.root,bd=4,bg='#dfe3ee',highlightbackground='#759fe6',highlightthickness=3) 
+        self.frame2.place(relx=0.02, rely=0.5, relwidth=0.96, relheight=0.46)       
 
 Application()
