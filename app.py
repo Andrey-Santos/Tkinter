8cd11f79dd3de1fef40255bcfead33bdd10eb0f4
Andrey C. Dos Santos <Andrey-Carlos@outlook.com>
2023-05-19 16:11:10 -0300

    Criado Função limpar

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from tkinter import ttk
 
 root = Tk()
 
-class Application():
+class Funcs():
+    def limpa_Tela(self):
+        self.codigo_entry.delete(0, END)
+        self.nome_entry.delete(0, END)
+        self.telefone_entry.delete(0, END)
+        self.cidade_entry.delete(0, END)
+
+class Application(Funcs):
     def __init__(self):
         self.root = root
         self.tela()
@@ -31,24 +38,24 @@ class Application():
 
     def widgets_frame1(self):
         ### Criação do botao limpar
-        self.bt_limpar = Button(self.frame_1, text= "Limpar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
+        self.bt_limpar = Button(self.frame_1, text= "Limpar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'),command=self.limpa_Tela)
         self.bt_limpar.place(relx= 0.2, rely=0.1, relwidth=0.1, relheight= 0.15)
 
         ### Criação do botao buscar
-        self.bt_limpar = Button(self.frame_1, text="Buscar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
+        self.bt_buscar = Button(self.frame_1, text="Buscar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
+        self.bt_buscar.place(relx=0.3, rely=0.1, relwidth=0.1, relheight=0.15)
 
         ### Criação do botao novo
-        self.bt_limpar = Button(self.frame_1, text="Novo", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
+        self.bt_novo = Button(self.frame_1, text="Novo", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
+        self.bt_novo.place(relx=0.6, rely=0.1, relwidth=0.1, relheight=0.15)
 
         ### Criação do botao alterar
-        self.bt_limpar = Button(self.frame_1, text="Alterar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
+        self.bt_alterar = Button(self.frame_1, text="Alterar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
+        self.bt_alterar.place(relx=0.7, rely=0.1, relwidth=0.1, relheight=0.15)
 
         ### Criação do botao apagar
-        self.bt_limpar = Button(self.frame_1, text="Apagar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
-        self.bt_limpar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
+        self.bt_apagar = Button(self.frame_1, text="Apagar", bd=2, bg = '#107db2',fg = 'white', font = ('verdana', 8, 'bold'))
+        self.bt_apagar.place(relx=0.8, rely=0.1, relwidth=0.1, relheight=0.15)
 
         ## Criação da label e entrada do codigo
         self.lb_codigo = Label(self.frame_1, text = "Código", bg= '#dfe3ee', fg = '#107db2')
@@ -68,15 +75,15 @@ class Application():
         self.lb_nome = Label(self.frame_1, text="Telefone", bg= '#dfe3ee', fg = '#107db2')
         self.lb_nome.place(relx=0.05, rely=0.6)
 
-        self.nome_entry = Entry(self.frame_1)
-        self.nome_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
+        self.telefone_entry = Entry(self.frame_1)
+        self.telefone_entry.place(relx=0.05, rely=0.7, relwidth=0.4)
 
         ## Criação da label e entrada da cidade
         self.lb_nome = Label(self.frame_1, text="Cidade", bg= '#dfe3ee', fg = '#107db2')
         self.lb_nome.place(relx=0.5, rely=0.6)
 
-        self.nome_entry = Entry(self.frame_1)
-        self.nome_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
+        self.cidade_entry = Entry(self.frame_1)
+        self.cidade_entry.place(relx=0.5, rely=0.7, relwidth=0.4)
 
     def lista_frame2(self):
         self.listaCli = ttk.Treeview(self.frame_2, height=3, column=("col1", "col2", "col3", "col4"))        
@@ -97,4 +104,5 @@ class Application():
         self.scroolLista = Scrollbar(self.frame_2, orient='vertical')
         self.listaCli.configure(yscroll=self.scroolLista.set)
         self.scroolLista.place(relx=0.96, rely=0.1, relwidth=0.04, relheight=0.85)
+
 Application()
